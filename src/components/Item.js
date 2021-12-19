c2e478b77092cf622aec9c61db5b6465550b7b8d
Hannah McCullough <hmac091@gmail.com>
2021-12-19 15:19:03 -0700

    commit tree 62faf60b9f46d621ce2efba46b367e35fbe09b3e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false);
+
+  function handleAddClick(e) {
+    setIsAdded((isAdded) => !isAdded);
+  }
+
   return (
-    <li className="">
+    <li className={isAdded ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddClick}>{isAdded ? 'Add to ' : 'Remove from '} Cart</button>
     </li>
   );
 }
